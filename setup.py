32af56d7ea0b429cfe9ff4fb0c6c02ffa0ac636d
Filip Jakovljevic <fillix96@gmail.com>
2024-05-28 15:18:10 +0200

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 import importlib
 import pathlib
-import os
 from setuptools.command.install import install
 from bs4 import BeautifulSoup
 
 # Function to patch Streamlit for ads
 def patch_streamlit_for_ads():
     print("Patching streamlit for ads")
+    
     # Loading Streamlit index.html
     streamlit_path = importlib.util.find_spec("streamlit").origin
     index_path = pathlib.Path(streamlit_path).parent / "static" / "index.html"
@@ -16,22 +16,22 @@ def patch_streamlit_for_ads():
     index_html = BeautifulSoup(html_content, "lxml")
 
     # Creating ads HTML
-    ads_html = """<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9139235018955761" crossorigin="anonymous"></script>"""
-    ads_tag = BeautifulSoup(ads_html, "html.parser")
+    script_tag = """<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9139235018955761" crossorigin="anonymous"></script>"""
+    script_tag = BeautifulSoup(ads_html, "html.parser")
+    meta_tag = """<meta name="google-adsense-account" content="ca-pub-9139235018955761">"""
+    meta_tag = BeautifulSoup(ads_html, "html.parser")
 
-    # If tag doesn't exist, add it to the header and save the file
-    if not index_html.find("script", src=ads_tag.script.get("src")):
-        index_html.head.append(ads_tag)
-        index_html_with_ads = index_html.prettify()
-        
-        os.chmod(index_path, 0o644)
-        index_path.write_text(index_html_with_ads)
-        print("Streamlit patched successfully.")
-    else:
-        print("Ads script already exists in Streamlit index.html.")
+    # adding tags to the header and save the file
+    index_html.head.append(script_tag)
+    index_html.head.append(meta_tag)
+    index_html_with_ads = index_html.prettify()
+    index_path.write_text(index_html_with_ads)
+    
+    print("Streamlit patched successfully.")
 
 class CustomInstallCommand(install):
     def run(self):
+        print("Installing streamlit_ads_patch")
         install.run(self)
         patch_streamlit_for_ads()
 
